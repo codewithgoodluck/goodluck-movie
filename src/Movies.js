0add9154bb1d0406b101384e245e7497d30fbeb9
codewithgoodluck <goodluckmordi4@gmail.com>
2022-10-19 21:55:51 +0100

    done

---
+++ src/Movies.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useGlobalContext } from './context'
+import { Link } from 'react-router-dom'
+const url =
+  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+
+const Movies = () => {
+  const { movies, isLoading } = useGlobalContext()
+  if (isLoading) {
+    return <div className="loading">
+
+    </div>
+  }
+  return <section className='movies'>
+    {movies.map((movie) => {
+      const { imdbID: id, Poster: poster, Title: title, Year: year } = movie
+      return <Link to={`/movies/${id}`} key={id} className="movie" >
+
+        <article>
+          <img src={poster} alt={title} />
+          <div className="movie-info">
+            <h3 className="title">{title}</h3>
+            <p>{year}</p>
+          </div>
+        </article>
+      </Link>
+    })}
+  </section>
+}
+
+export default Movies
